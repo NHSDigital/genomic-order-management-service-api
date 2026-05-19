4207d71cc683347cee436342963d69ba8ab92fd1
vgnapskainos <253046491+vgnapskainos@users.noreply.github.com>
2026-05-19 12:22:38 +0100

    fixed format and english check issues

--- infrastructure/environments/prod/main.tf
+++ infrastructure/environments/prod/main.tf
@@ -2,10 +2,10 @@
 module "nhs_e_secrets" {
   source = "../../modules/nhs-e-secrets"
 
-  project                   = var.project
-  region                    = var.region
-  environment               = var.environment
-  irs_role_arn              = var.irs_role_arn
+  project                    = var.project
+  region                     = var.region
+  environment                = var.environment
+  irs_role_arn               = var.irs_role_arn
   create_placeholder_secrets = var.create_placeholder_secrets
-  enable_kms_key_rotation   = var.enable_kms_key_rotation
+  enable_kms_key_rotation    = var.enable_kms_key_rotation
 }
