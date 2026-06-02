0762b4636e5a201c032f7207d9ada878cc92dc90
vgnapskainos <253046491+vgnapskainos@users.noreply.github.com>
2026-06-02 15:33:58 +0100

    debug

--- scripts/terraform/terraform.sh
+++ scripts/terraform/terraform.sh
@@ -54,10 +54,6 @@ function run-terraform-in-docker() {
   echo docker run --rm --platform linux/amd64 \
     --volume "$PWD":/workdir \
     --workdir /workdir \
-    -e AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
-    -e AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" \
-    -e AWS_SESSION_TOKEN="$AWS_SESSION_TOKEN" \
-    -e AWS_REGION="$AWS_REGION" \
     "$image" \
       $cmd
   echo $cmd
