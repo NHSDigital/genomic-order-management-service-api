3858361f9c591666ffe146ccab9e69ab2ca5cc80
vgnapskainos <253046491+vgnapskainos@users.noreply.github.com>
2026-06-09 14:18:42 +0100

    Deployment testing

--- infrastructure/environments/prod/main.tf
+++ infrastructure/environments/prod/main.tf
@@ -1,6 +1,8 @@
 # Production environment configuration
 #  intersystems_role_arn = var.intersystems_role_arn
 # the below must be replaced by InterSystems Role, added for testing
+data "aws_caller_identity" "current" {}
+
 module "nhs_e_secrets" {
   source                = "../../modules/nhs-e-secrets"
   project               = var.project
