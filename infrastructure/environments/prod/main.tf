c8a1f72c10315e71014da446d1a2d0334045997c
vgnapskainos <253046491+vgnapskainos@users.noreply.github.com>
2026-06-09 14:10:39 +0100

    Deployment testing

--- infrastructure/environments/prod/main.tf
+++ infrastructure/environments/prod/main.tf
@@ -1,13 +1,12 @@
 # Production environment configuration
+#  intersystems_role_arn = var.intersystems_role_arn
+# the below must be replaced by InterSystems Role, added for testing
 module "nhs_e_secrets" {
-  source = "../../modules/nhs-e-secrets"
-
+  source                = "../../modules/nhs-e-secrets"
   project               = var.project
   region                = var.region
   environment           = var.environment
-#  intersystems_role_arn = var.intersystems_role_arn
-# the below must be replaced by InterSystems Role, added for testing
-  intersystems_role_arn = arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+  intersystems_role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
   external_id           = var.external_id
   tags                  = var.tags
   secrets               = var.secrets
