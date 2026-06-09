20f707ef9c3fd11fb33c68a5afa9e5acf8f3a7c2
vgnapskainos <253046491+vgnapskainos@users.noreply.github.com>
2026-06-09 14:04:45 +0100

    Deployment testing

--- infrastructure/environments/prod/main.tf
+++ infrastructure/environments/prod/main.tf
@@ -5,7 +5,9 @@ module "nhs_e_secrets" {
   project               = var.project
   region                = var.region
   environment           = var.environment
-  intersystems_role_arn = var.intersystems_role_arn
+#  intersystems_role_arn = var.intersystems_role_arn
+# the below must be replaced by InterSystems Role, added for testing
+  intersystems_role_arn = arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
   external_id           = var.external_id
   tags                  = var.tags
   secrets               = var.secrets
